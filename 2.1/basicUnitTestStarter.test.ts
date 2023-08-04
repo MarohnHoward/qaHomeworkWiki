20470ae11bfa7deefed0ba29e607244a54945003
Kyle <kylesclawson@gmail.com>
2023-08-04 15:41:20 -0600

    added a gitignore, updated packages, and moved solutions from previous folder to here

--- 2.1/basicUnitTestStarter.test.ts
+++ 2.1/basicUnitTestStarter.test.ts
@@ -1,48 +1,56 @@
 describe("Testing the calculator", () => {
-    test("Example test", () => {
-      expect(calculator.add(2, 50)).toBe(52);
+  test("Example test", () => {
+    expect(calculator.add(2, 50)).toBe(52);
+  });
+  test("Addition works", () => {
+    expect(calculator.add(27, 33)).toBe(60);
+  });
+  test("Subtraction works", () => {
+    expect(calculator.subtract(10, 5)).toBe(5);
+  });
+  test("Multiplication works", () => {
+    expect(calculator.multiply(6, 12)).toBe(72);
+  });
+  test("Division works", () => {
+    expect(calculator.divide(100, 10)).toBe(10);
+  });
+  describe("Stretch Goals", () => {
+    test("(2+2)/4 == 1", () => {
+      expect(calculator.divide(calculator.add(2, 2), 4)).toBe(1);
     });
-    test("Addition works", () => {
-      expect(true).toBeTruthy();
-    });
-    test("Subtraction works", () => {
-      expect(true).toBeTruthy();
-    });
-    test("Multiplication works", () => {
-      expect(true).toBeTruthy();
-    });
-    test("Division works", () => {
-      expect(true).toBeTruthy();
-    });
-    describe("Stretch Goals", () => {
-      test("(2+2)/4 == 1", () => {
-        expect(calculator.divide(calculator.add(2, 2), 4)).toBe(1);
-      });
-      test("(28/4)*(6/2)*(30-27+(3-4)) == 42", () => {
-        expect(true).toBeTruthy();
-      });
+    test("(28/4)*(6/2)*(30-27+(3-4)) == 42", () => {
+      expect(
+        calculator.multiply(
+          calculator.multiply(
+            calculator.divide(28, 4),
+            calculator.divide(6, 2)
+          ),
+          calculator.add(calculator.subtract(30, 27), calculator.subtract(3, 4))
+        )
+      ).toBe(42);
     });
   });
-  
-  const calculator = {
-    /** Takes two numbers and adds them together.
-     * @example
-     * calculator.add(2, 3) == 5
-     */
-    add: (a: number, b: number): number => a + b,
-    /** Takes two numbers and subtracts the second from the first.
-     * @example
-     * calculator.subtract(2, 3) == -1
-     */
-    subtract: (a: number, b: number): number => a - b,
-    /** Takes two numbers and multiplies them together.
-     * @example
-     * calculator.multiply(2, 3) == 6
-     */
-    multiply: (a: number, b: number): number => a * b,
-    /** Takes two numbers and divides the first by the second.
-     * @example
-     * calculator.divide(2, 3) == 0.5
-     */
-    divide: (a: number, b: number): number => a / b,
-  };
+});
+
+const calculator = {
+  /** Takes two numbers and adds them together.
+   * @example
+   * calculator.add(2, 3) == 5
+   */
+  add: (a: number, b: number): number => a + b,
+  /** Takes two numbers and subtracts the second from the first.
+   * @example
+   * calculator.subtract(2, 3) == -1
+   */
+  subtract: (a: number, b: number): number => a - b,
+  /** Takes two numbers and multiplies them together.
+   * @example
+   * calculator.multiply(2, 3) == 6
+   */
+  multiply: (a: number, b: number): number => a * b,
+  /** Takes two numbers and divides the first by the second.
+   * @example
+   * calculator.divide(2, 3) == 0.5
+   */
+  divide: (a: number, b: number): number => a / b,
+};
--- 2.4/homeworkTest.test.ts
+++ 2.4/homeworkTest.test.ts
@@ -1,18 +1,18 @@
 import {
-    Builder,
-    By,
-    Capabilities,
-    until,
-    WebDriver,
-    WebElement,
-    Key,
+  Builder,
+  By,
+  Capabilities,
+  until,
+  WebDriver,
+  WebElement,
+  Key,
 } from "selenium-webdriver";
 
 const chromedriver = require("chromedriver");
 
 const driver: WebDriver = new Builder()
-    .withCapabilities(Capabilities.chrome())
-    .build();
+  .withCapabilities(Capabilities.chrome())
+  .build();
 const bernice: By = By.name("employee1");
 const marnie: By = By.name("employee2");
 const phillip: By = By.name("employee3");
@@ -25,147 +25,146 @@ const cancelButton: By = By.name("cancel");
 const errorCard: By = By.css(".errorCard");
 
 describe("Employee Manager 1.2", () => {
-
-    beforeEach(async () => {
-        await driver.get(
-        "https://devmountain-qa.github.io/employee-manager/1.2_Version/index.html"
-        );
+  beforeEach(async () => {
+    await driver.get(
+      "https://devmountain-qa.github.io/employee-manager/1.2_Version/index.html"
+    );
+  });
+  afterAll(async () => {
+    await driver.quit();
+  });
+  describe("handles unsaved, canceled, and saved changes correctly", () => {
+    test("An unsaved change doesn't persist", async () => {
+      /*
+          This test follows these steps:
+          1. Open Bernice Ortiz
+          2. Edit the name input
+          3. Open Phillip Weaver
+          4. Open Bernice Ortiz
+          5. Verify the name field is the original name
+          */
+      await driver.findElement(bernice).click();
+      await driver.wait(
+        until.elementIsVisible(await driver.findElement(nameInput))
+      );
+      await driver.findElement(nameInput).clear();
+      await driver.findElement(nameInput).sendKeys("Test Name");
+      await driver.findElement(phillip).click();
+      await driver.wait(
+        until.elementTextContains(
+          await driver.findElement(nameDisplay),
+          "Phillip"
+        )
+      );
+      await driver.findElement(bernice).click();
+      await driver.wait(
+        until.elementTextContains(
+          await driver.findElement(nameDisplay),
+          "Bernice"
+        )
+      );
+      expect(
+        await (await driver.findElement(nameInput)).getAttribute("value")
+      ).toBe("Bernice Ortiz");
     });
-    afterAll(async () => {
-        await driver.quit();
-    });
-    describe("handles unsaved, canceled, and saved changes correctly", () => {
-        test("An unsaved change doesn't persist", async () => {
-        /*
-        This test follows these steps:
-        1. Open Bernice Ortiz
-        2. Edit the name input
-        3. Open Phillip Weaver
-        4. Open Bernice Ortiz
-        5. Verify the name field is the original name
-        */
-        await driver.findElement().click();
-        await driver.wait(
-            until.elementIsVisible(await driver.findElement())
-        );
-        await driver.findElement().clear();
-        await driver.findElement().sendKeys("Test Name");
-        await driver.findElement().click();
-        await driver.wait(
-            until.elementTextContains(
-            await driver.findElement(),
-            "Phillip"
-            )
-        );
-        await driver.findElement().click();
-        await driver.wait(
-            until.elementTextContains(
-            await driver.findElement(),
-            "Bernice"
-            )
-        );
-        expect(
-            await (await driver.findElement()).getAttribute("")
-        ).toBe("");
-        });
 
-        test("A canceled change doesn't persist", async () => {
-            /*
-            This test follows these steps:
-            1. Open Phillip Weaver
-            2. Edit the name input
-            3. Click cancel
-            5. Verify the name field is the original name
-            */
-            await driver.findElement().click();
-            await driver.wait(
-                until.elementIsVisible(await driver.findElement())
-            );
-            await driver.findElement().clear();
-            await driver.findElement().sendKeys("Test Name");
-            await driver.findElement().click();
-            expect(
-                await (await driver.findElement()).getAttribute("")
-            ).toBe("");
-        });
-
-        test("A saved change persists", async () => {
-            /*
-            This test follows these steps:
-            1. Open Bernice Ortiz
-            2. Edit the name input
-            3. Save the change
-            4. Open Phillip Weaver
-            5. Open Bernice Ortiz's old record
-            5. Verify the name field is the edited name
-            */
-            await driver.findElement().click();
-            await driver.wait(
-                until.elementIsVisible(await driver.findElement())
-            );
-            await driver.findElement().clear();
-            await driver.findElement().sendKeys("Test Name");
-            await driver.findElement().click();
-            await driver.findElement().click();
-            await driver.wait(
-                until.elementTextContains(
-                await driver.findElement(),
-                "Phillip"
-                )
-            );
-            await driver.findElement().click();
-            expect(
-                await (await driver.findElement()).getAttribute("value")
-            ).toBe("Bernice Ortiz");
+    test("A canceled change doesn't persist", async () => {
+      /*
+              This test follows these steps:
+              1. Open Phillip Weaver
+              2. Edit the name input
+              3. Click cancel
+              5. Verify the name field is the original name
+              */
+      await driver.findElement(phillip).click();
+      await driver.wait(
+        until.elementIsVisible(await driver.findElement(nameInput))
+      );
+      await driver.findElement(nameInput).clear();
+      await driver.findElement(nameInput).sendKeys("Test Name");
+      await driver.findElement(cancelButton).click();
+      expect(
+        await (await driver.findElement(nameInput)).getAttribute("value")
+      ).toBe("Phillip Weaver");
     });
-});
 
-    describe("handles error messages correctly", () => {
-        test("shows an error message for an empty name field", async () => {
-            /*
-            This test follows these steps:
-            1. Open Bernice Ortiz
-            2. Clear the name input
-            3. Save the change
-            4. Verify the error is present
-            */
-            await driver.findElement().click();
-            await driver.wait(
-                until.elementIsVisible(await driver.findElement())
-            );
-            await driver.findElement().clear();
-            await driver.findElement().sendKeys(Key.SPACE, Key.BACK_SPACE);
-            await driver.findElement().click();
-            await driver.wait(until.elementLocated());
-            expect(await (await driver.findElement()).getText()).toBe(
-                "The name field must be between 1 and 30 characters long."
-            );
-        });
-        test("lets you cancel out of an error message", async () => {
-            /*
-            This test follows these steps:
-            1. Open Bernice Ortiz
-            2. Clear the name input
-            3. Save the change
-            4. Verify the error is present
-            5. Cancel the change
-            6. Verify the error is gone
-            */
-            await driver.findElement().click();
-            await driver.wait(
-                until.elementIsVisible(await driver.findElement())
-            );
-            await driver.findElement().clear();
-            await driver.findElement().sendKeys(Key.SPACE, Key.BACK_SPACE);
-            await driver.findElement().click();
-            await driver.wait(until.elementLocated());
-            expect(await (await driver.findElement()).getText()).toBe(
-                "The name field must be between 1 and 30 characters long."
-            );
-            await driver.findElement().sendKeys(Key.SPACE);
-            await driver.findElement().click();
-            driver.wait(() => true, 500);
-            expect(await driver.findElements()).toHaveLength(0);
-        });
+    test("A saved change persists", async () => {
+      /*
+              This test follows these steps:
+              1. Open Bernice Ortiz
+              2. Edit the name input
+              3. Save the change
+              4. Open Phillip Weaver
+              5. Open Bernice Ortiz's old record
+              5. Verify the name field is the edited name
+              */
+      await driver.findElement(bernice).click();
+      await driver.wait(
+        until.elementIsVisible(await driver.findElement(nameInput))
+      );
+      await driver.findElement(nameInput).clear();
+      await driver.findElement(nameInput).sendKeys("Test Name");
+      await driver.findElement(saveButton).click();
+      await driver.findElement(phillip).click();
+      await driver.wait(
+        until.elementTextContains(
+          await driver.findElement(nameDisplay),
+          "Phillip"
+        )
+      );
+      await driver.findElement(bernice).click();
+      expect(
+        await (await driver.findElement(nameInput)).getAttribute("value")
+      ).toBe("Test Name");
     });
+  });
+
+  describe("handles error messages correctly", () => {
+    test("shows an error message for an empty name field", async () => {
+      /*
+              This test follows these steps:
+              1. Open Bernice Ortiz
+              2. Clear the name input
+              3. Save the change
+              4. Verify the error is present
+              */
+      await driver.findElement(bernice).click();
+      await driver.wait(
+        until.elementIsVisible(await driver.findElement(nameInput))
+      );
+      await driver.findElement(nameInput).clear();
+      await driver.findElement(nameInput).sendKeys(Key.SPACE, Key.BACK_SPACE);
+      await driver.findElement(saveButton).click();
+      await driver.wait(until.elementLocated(errorCard));
+      expect(await (await driver.findElement(errorCard)).getText()).toBe(
+        "The name field must be between 1 and 30 characters long."
+      );
+    });
+    test("lets you cancel out of an error message", async () => {
+      /*
+              This test follows these steps:
+              1. Open Bernice Ortiz
+              2. Clear the name input
+              3. Save the change
+              4. Verify the error is present
+              5. Cancel the change
+              6. Verify the error is gone
+              */
+      await driver.findElement(bernice).click();
+      await driver.wait(
+        until.elementIsVisible(await driver.findElement(nameInput))
+      );
+      await driver.findElement(nameInput).clear();
+      await driver.findElement(nameInput).sendKeys(Key.SPACE, Key.BACK_SPACE);
+      await driver.findElement(saveButton).click();
+      await driver.wait(until.elementLocated(errorCard));
+      expect(await (await driver.findElement(errorCard)).getText()).toBe(
+        "The name field must be between 1 and 30 characters long."
+      );
+      await driver.findElement(nameInput).sendKeys(Key.SPACE);
+      await driver.findElement(saveButton).click();
+      driver.wait(() => true, 500);
+      expect(await driver.findElements(errorCard)).toHaveLength(0);
+    });
+  });
 });
